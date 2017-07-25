6a9d0533d2e1ca9cac12c7415bc0580d42d6a722
noiless <muuunbyul@gmail.com>
2017-07-25 12:59:11 +0900

    add gitignore

--- Classes/OverScene.cpp
+++ Classes/OverScene.cpp
@@ -1,4 +1,5 @@
 #include "OverScene.h"
+#include "GameScene.h"
 
 USING_NS_CC;
 
@@ -25,7 +26,26 @@ bool GameOver::init()
 	cocos2d::Label * label1 = Label::createWithTTF("GAME OVER", "fonts/arial.ttf", 30);
 	label1->setPosition(Vec2(origin.x + visibleSize.width / 2, origin.y + visibleSize.height / 2));
 
+	cocos2d::Label * label2 = Label::createWithTTF("Press z to restart", "fonts/arial.ttf", 20);
+	label2->setPosition(Vec2(origin.x + visibleSize.width / 2, origin.y + visibleSize.height / 3));
+
 	this->addChild(label1);
+	this->addChild(label2);
+
+	auto eventListener = EventListenerKeyboard::create();
+
+	eventListener->onKeyPressed = [=](EventKeyboard::KeyCode keyCode, Event* event) {
+		//°ÔÀÓ ¾ÀÀ¸·Î ¾À º¯°æ
+		if (keyCode == EventKeyboard::KeyCode::KEY_Z) {
+
+			auto gamScene = GameWorld::createScene();
+			Director::getInstance()->replaceScene(gamScene);
+
+		}
+
+	};
+
+	this->_eventDispatcher->addEventListenerWithSceneGraphPriority(eventListener, this);
 
 	return true;
 
